33093e68b24c1425905233e95e5ed8decc6e4c18
Mohamed Abdul Haseeb <65020831+abdhaseeb@users.noreply.github.com>
2026-03-08 08:05:43 +0000

    First commit for arrow.js

---
+++ arrow.js
@@ -0,0 +1,33 @@
+// let myName = "placeholder"
+// let age = 0
+
+// const obj = {
+//     myName : "Haseeb",
+//     age : 27, 
+//     printDetails : () => {
+//         console.log(`Name is ${this.myName} and age is ${this.age}`); 
+//     }
+// }
+
+// obj.printDetails()
+
+// const obj = {
+//   name: "Haseeb",
+//   method() {
+//     return () => {
+//       console.log(this.name);
+//     };
+//   }
+// };
+
+// const fn = obj.method();
+// fn();
+
+const obj = {
+  name: "Haseeb",
+  greet() {
+    console.log(this.name);
+  }
+};
+
+setTimeout(obj.greet.bind(obj), 1000);
