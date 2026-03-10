ea3e8422edf0de38fea64dfe283c041051a4be06
Mohamed Abdul Haseeb <65020831+abdhaseeb@users.noreply.github.com>
2026-03-10 11:24:41 +0000

    start new game logic added

--- DOM/dom-projects/04-number-guess/numberGuesser.js
+++ DOM/dom-projects/04-number-guess/numberGuesser.js
@@ -1,8 +1,11 @@
 const form = document.querySelector('form');
-const randomNumber = Math.floor(Math.random() * 100) + 1;
+let randomNumber = Math.floor(Math.random() * 100) + 1;
 
 let counter = 10;
 let gameOver = false;
+let gamesPlayed = 0;
+let wins = 0;
+let losses = 0;
 
 const previousGuesses = [];
 
@@ -23,14 +26,9 @@ form.addEventListener('submit', (event) => {
 
   if (randomNumber === guessInput) {
     resultDisplay.textContent = 'Correct!';
-    gameOver = true;
-    document.querySelector('#guessField').disabled = true;
-    return;
-  } else if (guessInput > randomNumber) {
-    resultDisplay.textContent = 'Try lower';
-  } else {
-    resultDisplay.textContent = 'Try higher';
+    endGame();
   }
+  resultDisplay.textContent = guessInput > randomNumber ? 'Try lower' : 'Try higher';
 
   //updating counter everytime submit is hit and updated counter is shown on page
   counter--;
@@ -46,18 +44,38 @@ form.addEventListener('submit', (event) => {
 function endGame() {
   gameOver = true;
 
-  const input = document.querySelector('#guessField');
-  input.disabled = true;
+  const guessInput = document.querySelector('#guessField');
+  guessInput.disabled = true;
 
   const restartButton = document.createElement('button');
   restartButton.textContent = 'Play Again';
-  restartButton.attributes('id', 'restartGame');
+  restartButton.setAttribute('id', 'restartGame');
   // restartButton.id = 'restartGame';
 
   const wrapper = document.querySelector('#wrapper');
-  wrapper.appendChild(restartButton);
+  if (!document.querySelector('#restartGame')) {
+    wrapper.appendChild(restartButton);
+  }
 
   restartButton.addEventListener('click', newGame);
 }
 
-function newGame() {}
+function newGame() {
+  randomNumber = Math.floor(Math.random() * 100) + 1;
+  counter = 10;
+  gameOver = false;
+
+  previousGuesses.length = 0;
+
+  document.querySelector('.guesses').textContent = '';
+  document.querySelector('.lastResult').textContent = counter;
+  document.querySelector('.lowOrHi').textContent = '';
+
+  const guessInput = document.querySelector('#guessField');
+  guessInput.disabled = false;
+  guessInput.value = '';
+
+  document.querySelector('#restartGame').remove();
+}
+
+//ayoooo
